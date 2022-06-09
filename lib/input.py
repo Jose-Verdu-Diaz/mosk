440e200dbd436954771ca7b085ee7f14a93b4d45
Jose-Verdu_Diaz <pepv@hotmail.es>
2022-06-09 09:09:17 +0100

    Keyboard input for project explorer

--- lib/input.py
+++ lib/input.py
@@ -12,6 +12,19 @@ def inp_str(prompt:str=None, options:list=None):
     return inp
 
 
+def check_int(inp, options:list=None):
+    try: inp = int(inp)
+    except Exception: 
+        return None
+
+    if options: 
+        if inp not in options: 
+            input(f'The string should be one of the following: {options}. Press Enter to continue...')
+            return None
+
+    return inp
+
+
 def inp_int(prompt:str=None, options:list=None, exit=True):
     try: 
         if not prompt: prompt = 'Enter an option: '
@@ -23,14 +36,4 @@ def inp_int(prompt:str=None, options:list=None, exit=True):
     if exit: 
         if inp == 'e': return inp
 
-    try: inp = int(inp)
-    except Exception: 
-        input('Unexpected input. Press Enter to continue...')
-        return None
-
-    if options: 
-        if inp not in options: 
-            input(f'The string should be one of the following: {options}. Press Enter to continue...')
-            return None
-
-    return inp
+    check_int(inp, options)
--- mosk.py
+++ mosk.py
@@ -3,8 +3,8 @@ import sys
 import time
 from termios import tcflush, TCIFLUSH
 
-from lib.keyboard import read_key, on_press_key
-from lib.input import inp_str, inp_int
+from lib.keyboard import read_key
+from lib.input import inp_str, inp_int, check_int
 from lib.gui import Clr, banner, main_menu, clear, new_menu, new_project, new_task, projects_menu, project_info
 from models.Project import Project
 from models.State import State
@@ -27,62 +27,67 @@ def main():
                 kwargs = {'project_names': state.prj_list()}
                 print_list[-1] = projects_menu
                 for p in print_list: p(**kwargs)
-                inp = inp_int(options=list(range(len(state.projects))))
+                inp = read_key()
+                #inp = inp_int(options=list(range(len(state.projects))))
                 if inp == None: continue
                 elif inp == 'e': break
 
-                # Project Info
-                else:
-                    proj_id = inp
-                    kwargs = {'selected_task': 0}
-                    while True:
-                        kwargs['project_info'] = state.projects[proj_id].project_info()
-                        print_list[-1] = project_info
-                        for p in print_list: p(**kwargs)
+                else: 
+                    inp = check_int(inp, options=list(range(len(state.projects))))
+                    if inp == None: continue
 
-                        inp = read_key()
-                        if inp == None: continue
-                        elif inp == 'e': break
-
-                        # Project Info New Task
-                        elif inp == 'n':
-                            kwargs['name'], kwargs['description'] = '', ''
-                            while True:
-                                print_list[-1] = new_task
-                                for p in print_list: p(**kwargs)
-                                inp = read_key()
-                                if inp == None: continue
-                                elif inp == 'e': break
-
-                                # Project Info New Task: Name
-                                elif inp == 'n':
-                                    for p in print_list: p(**kwargs)
-                                    kwargs['name'] = inp_str(prompt='Enter name: ')
-
-                                # Project Info New Task: Description
-                                elif inp == 'd':
-                                    for p in print_list: p(**kwargs)
-                                    kwargs['description'] = inp_str(prompt='Enter description: ')
-
-                                # Project Info New Task: Create
-                                elif inp == 'c':
-                                    state.projects[proj_id].new_task(**kwargs)
-                                    break
-
-                        elif inp == 'c': 
-                            state.projects[proj_id].complete_task(kwargs['selected_task'])
+                    # Project Info
+                    else:
+                        proj_id = inp
+                        kwargs = {'selected_task': 0}
+                        while True:
                             kwargs['project_info'] = state.projects[proj_id].project_info()
+                            print_list[-1] = project_info
+                            for p in print_list: p(**kwargs)
 
-                        elif inp == 'i': 
-                            state.projects[proj_id].complete_task(kwargs['selected_task'])
-                            kwargs['project_info'] = state.projects[proj_id].project_info()
+                            inp = read_key()
+                            if inp == None: continue
+                            elif inp == 'e': break
 
-                        elif inp == 'up':
-                            if kwargs['selected_task'] > 0: 
-                                kwargs['selected_task'] -= 1
-                        elif inp == 'down':
-                            if kwargs['selected_task'] < len(state.projects[proj_id].tasks) - 1: 
-                                kwargs['selected_task'] += 1
+                            # Project Info New Task
+                            elif inp == 'n':
+                                kwargs['name'], kwargs['description'] = '', ''
+                                while True:
+                                    print_list[-1] = new_task
+                                    for p in print_list: p(**kwargs)
+                                    inp = read_key()
+                                    if inp == None: continue
+                                    elif inp == 'e': break
+
+                                    # Project Info New Task: Name
+                                    elif inp == 'n':
+                                        for p in print_list: p(**kwargs)
+                                        kwargs['name'] = inp_str(prompt='Enter name: ')
+
+                                    # Project Info New Task: Description
+                                    elif inp == 'd':
+                                        for p in print_list: p(**kwargs)
+                                        kwargs['description'] = inp_str(prompt='Enter description: ')
+
+                                    # Project Info New Task: Create
+                                    elif inp == 'c':
+                                        state.projects[proj_id].new_task(**kwargs)
+                                        break
+
+                            elif inp == 'c': 
+                                state.projects[proj_id].complete_task(kwargs['selected_task'])
+                                kwargs['project_info'] = state.projects[proj_id].project_info()
+
+                            elif inp == 'i': 
+                                state.projects[proj_id].complete_task(kwargs['selected_task'])
+                                kwargs['project_info'] = state.projects[proj_id].project_info()
+
+                            elif inp == 'up':
+                                if kwargs['selected_task'] > 0: 
+                                    kwargs['selected_task'] -= 1
+                            elif inp == 'down':
+                                if kwargs['selected_task'] < len(state.projects[proj_id].tasks) - 1: 
+                                    kwargs['selected_task'] += 1
 
         # Incubator
         elif inp == 'i':
